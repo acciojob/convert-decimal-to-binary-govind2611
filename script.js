465c5a514824c2bb3c860ab14def2c42e1971a93
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-10-16 08:33:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function decimalToBinary(num) {
-  // your code here
+  // Check if the input is 0
+  if (num === 0) {
+    return '0';
+  }
+
+  // Initialize an empty string to store the binary representation
+  let binary = '';
+
+  // Convert the decimal number to binary
+  while (num > 0) {
+    // Append the remainder of the division by 2 to the binary string
+    binary = (num % 2) + binary;
+    // Integer division by 2
+    num = Math.floor(num / 2);
+  }
+
+  return binary;
 }
+
 const num = parseInt(prompt("Enter a number."));
 alert(decimalToBinary(num));
